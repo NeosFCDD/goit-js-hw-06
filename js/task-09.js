a25ed32f5d2a40ccdef1ea5e983efc3e8266e9cd
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2022-12-28 17:16:07 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const textColorName = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeColorNow);
+
+function changeColorNow(){
+  bodyEl.style.backgroundColor = getRandomHexColor ();
+  textColorName.textContent = getRandomHexColor ();
+}
