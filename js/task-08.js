0a9db0ca2c8b0fb358c92b5a3aa70be6e063626e
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2022-12-28 17:00:57 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault()
+    const {
+        elements: {email, password},
+    } = event.currentTarget
+    
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all fields!')
+    }
+
+    const userData = {email: email.value, password: password.value}
+
+    console.log (userData)
+    event.currentTarget.reset()
+}
